337c2e16414c399046286cac4d995cdfce72671d
Strange From <strange.from@gmail.com>
2019-11-08 10:52:53 +0100

    -pl

--- deploy/entrypoint.sh
+++ deploy/entrypoint.sh
@@ -3,7 +3,7 @@
 : ${DOTNET_LAMBDA_PACKAGE_NAME:=latest.zip}
 export PATH="$PATH:/root/.dotnet/tools"
 cd "${DOTNET_LAMBDA_WORKING_DIR:-.}"
-dotnet lambda package $DOTNET_LAMBDA_PACKAGE_NAME --pl ./CustomAuthorizer
+dotnet lambda package $DOTNET_LAMBDA_PACKAGE_NAME -pl CustomAuthorizer
 #aws s3 cp --only-show-errors $DOTNET_LAMBDA_PACKAGE_NAME s3://$DOTNET_LAMBDA_S3_LOCATION/$DOTNET_LAMBDA_PACKAGE_NAME
 #aws lambda update-function-code \
 #  --region $AWS_REGION \
